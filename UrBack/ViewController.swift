5770c11fdd4163f827e26963a07b2b83af01306e
Alex <oleksandr>
2019-08-21 16:00:23 +0300

    CellsDoneOutletsImplemented

--- UrBack/ViewController.swift
+++ UrBack/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var topTextLabel: UILabel!
+    @IBOutlet weak var daysCounterLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var intervalLabel: UILabel!
+    @IBOutlet weak var intervalTimeLabel: UILabel!
+    @IBOutlet weak var decreaseIntervalButton: UIButton!
+    @IBOutlet weak var increaseIntervalButton: UIButton!
+    @IBOutlet weak var minutesLabel: UILabel!
+    @IBOutlet weak var scheduleLabel: UILabel!
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var toLabel: UILabel!
+    @IBOutlet weak var fromTimeButton: UIButton!
+    @IBOutlet weak var toTimeButton: UIButton!
+    @IBOutlet weak var activateLabel: UILabel!
+    @IBOutlet weak var activateSwitch: UISwitch!
+    @IBOutlet weak var saveChangesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
